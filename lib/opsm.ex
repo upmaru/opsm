2c1710fff9ff5b1e86f64643fd11e4fd90fc67b2
Zack Siri <me@zacksiri.com>
2025-02-09 10:06:57 +0700

    initial commit

---
+++ lib/opsm.ex
@@ -0,0 +1,18 @@
+defmodule Opsm do
+  @moduledoc """
+  Documentation for `Opsm`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Opsm.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/opsm_test.exs
@@ -0,0 +1,8 @@
+defmodule OpsmTest do
+  use ExUnit.Case
+  doctest Opsm
+
+  test "greets the world" do
+    assert Opsm.hello() == :world
+  end
+end
